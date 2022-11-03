cf6647c7390507664fae9f0673461fa2dd95c0e4
${nombreAleatorio} <n0mbr3al3at0r10@pm.me>
2022-11-03 15:35:12 -0300

    creating course schema and dto
    closes #14

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCourseDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateCourseDto {
+  @ApiProperty({
+    example: 'English for career development',
+  })
+  readonly name: string;
+
+  @ApiProperty({
+    example:
+      'In this course, you will learn about the job search, application, and interview process in the United States, while comparing and contrasting the same process in your home country. This course will also give you the opportunity to explore your global career path, while building your vocabulary and improving your language skills to achieve your professional goals.',
+  })
+  readonly description: string;
+
+  @ApiProperty({
+    example: 'Intermediate',
+  })
+  readonly level: string;
+
+  @ApiProperty({ example: 'English' })
+  readonly language: string;
+
+  @ApiProperty({ example: ['English', 'Language', 'Carrer development'] })
+  readonly keywords: string[];
+}
--- src/courses/schemas/course.schema.ts
+++ src/courses/schemas/course.schema.ts
@@ -1,1 +1,24 @@
-export class Course {}
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CourseDocument = Course & Document;
+
+@Schema()
+export class Course {
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  level: string;
+
+  @Prop()
+  language: string;
+
+  @Prop([String])
+  keywords: string[];
+}
+
+export const CourseSchema = SchemaFactory.createForClass(Course);
