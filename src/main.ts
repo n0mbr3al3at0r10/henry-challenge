e54a00d4c3de3648c0158305a07a9ffe24fd0d34
${nombreAleatorio} <n0mbr3al3at0r10@pm.me>
2022-11-03 11:34:01 -0300

    configuring swagger
    closes #13

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuring Swagger documentation options
+  const options = new DocumentBuilder()
+    .setTitle('Henry Challenge - Courses')
+    .setDescription('API REST for new courses functionality by Agustín Prado.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // Documentation route
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
