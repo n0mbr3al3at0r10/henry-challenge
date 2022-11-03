ab9666457e2a534c3ae98f5fbefc09f057ebdc73
${nombreAleatorio} <n0mbr3al3at0r10@pm.me>
2022-11-03 15:26:36 -0300

    configuring mongoose
    closes #12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/henry-challenge'),
+    CoursesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
