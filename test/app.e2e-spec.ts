1cd9e2a9b256f6794999d0c922cce26db8c0f529
${nombreAleatorio} <n0mbr3al3at0r10@pm.me>
2022-11-03 23:33:31 -0300

    adjusting e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        'Welcome to the API REST for new courses functionality by Agustín Prado.',
+      );
   });
 });
